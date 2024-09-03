f872f11540fd7739c990e3ddc3616f0543c9ef69
ANGEL ALEXANDER RAMÍREZ NAVARRO <angel.ramirez5119@alumnos.udg.mx>
2024-09-03 16:43:21 -0600

    Formulario hecho en clase

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+</head>
+<body>
+    <form action="/guardar-formulario" method="POST">
+        @csrf
+        <h1>Formulario de contacto</h1>
+        <div>
+            <label for="nombre"> Nombre: </label>
+            <input type="text" name="nombre"> 
+        </div>
+
+        <div>
+            <label for="correo">Correo: </label>
+            <input type="email" name="correo">
+        </div>
+
+
+        <div>
+            <label for="mensaje">Mensaje</label>
+            <textarea name="mensaje" id="mensaje" rows="10"></textarea>
+        </div>
+
+        <div>
+            <button id="botonEnviar" type="submit">Enviar</button>
+        </div>
+    </form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
+Route::post('/guardar-formulario', function (Request $request) {
+    //recibir datos
+    //dd es para matar la app y arrojar un mensaje 
+    dd($request->all());
+    dd($request->all(), $request->nombre);
+
+
+    //validar datos
+
+    //guardar datos
+
+    //redireccionar
 });
